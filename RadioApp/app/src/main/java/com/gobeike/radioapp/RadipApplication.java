ad2c19f5b619ee57cb988e3ac279c4faf6f69134
1049884729 <1049884729@qq.com>
2017-04-01 12:17:29 +0800

    添加代码格式化模板

---
+++ RadioApp/app/src/main/java/com/gobeike/radioapp/RadipApplication.java
@@ -0,0 +1,8 @@
+package com.gobeike.radioapp;
+
+/**
+ * Created by xuff on 17-4-1.
+ */
+
+public class RadipApplication {
+}
---
+++ RadioApp/app/src/main/java/com/gobeike/radioapp/music/PlayerMusicList.java
@@ -0,0 +1,8 @@
+package com.gobeike.radioapp.music;
+
+/**
+ * Created by xuff on 17-3-31.
+ */
+
+public class PlayerMusicList {
+}
